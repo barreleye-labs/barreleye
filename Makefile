3306f3c4399a0d762405b62999f369cb7c76a579
usiyoung <usiyoung7@gmail.com>
2023-04-27 23:44:03 +0900

    Feat: Transport layer

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+		go build  -o ./bin/barreleye
+
+run: build
+		./bin/barreleye
+		
+test: 
+		go test -v ./...
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/barreleye-labs/barreleye/network"
 )
 
 func main() {
-	fmt.Println("hello")
+	trLocal := network.NewLocalTransport("LOCAL")
+	trRemote := network.NewLocalTransport("REMOTE")
+
+	trLocal.Connect(trRemote)
+	trRemote.Connect(trLocal)
+
+	go func() {
+		for {
+			trRemote.SendMessage(trLocal.Addr(), []byte("hello world"))
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	opts := network.ServerOpts{
+		Transports: []network.Transport{trLocal},
+	}
+
+	s := network.NewServer(opts)
+	s.Start()
 }
