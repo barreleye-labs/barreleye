43f3df187365272c63008753c4a7db3213b90b28
k930503 <k930503@gmail.com>
2023-09-23 15:36:00 +0900

    Feat: Broadcast block

---
+++ network/util/random.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/barreleye-labs/barreleye/core"
+	"github.com/barreleye-labs/barreleye/crypto"
+	"github.com/barreleye-labs/barreleye/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func RandomBytes(size int) []byte {
+	token := make([]byte, size)
+	rand.Read(token)
+	return token
+}
+
+func RandomHash() types.Hash {
+	return types.HashFromBytes(RandomBytes(32))
+}
+
+// NewRandomTransaction return a new random transaction whithout signature.
+func NewRandomTransaction(size int) *core.Transaction {
+	return core.NewTransaction(RandomBytes(size))
+}
+
+func NewRandomTransactionWithSignature(t *testing.T, privKey crypto.PrivateKey, size int) *core.Transaction {
+	tx := NewRandomTransaction(size)
+	assert.Nil(t, tx.Sign(privKey))
+	return tx
+}
+
+func NewRandomBlock(t *testing.T, height uint32, prevBlockHash types.Hash) *core.Block {
+	txSigner := crypto.GeneratePrivateKey()
+	tx := NewRandomTransactionWithSignature(t, txSigner, 100)
+	header := &core.Header{
+		Version:       1,
+		PrevBlockHash: prevBlockHash,
+		Height:        height,
+		Timestamp:     time.Now().UnixNano(),
+	}
+	b, err := core.NewBlock(header, []*core.Transaction{tx})
+	assert.Nil(t, err)
+	dataHash, err := core.CalculateDataHash(b.Transactions)
+	assert.Nil(t, err)
+	b.Header.DataHash = dataHash
+
+	return b
+}
+
+func NewRandomBlockWithSignature(t *testing.T, pk crypto.PrivateKey, height uint32, prevHash types.Hash) *core.Block {
+	b := NewRandomBlock(t, height, prevHash)
+	assert.Nil(t, b.Sign(pk))
+
+	return b
+}
