15b081fbb5c0912bd885215e882c1f3003162517
k930503 <k930503@gmail.com>
2024-01-22 23:35:48 +0900

    Feat: Add get_last_block

--- core/blockchain_reader.go
+++ core/blockchain_reader.go
@@ -9,3 +9,8 @@ func (bc *Blockchain) GetBlockFromDB(hash common.Hash, number uint64) *Block {
 	query.GetBlock()
 	return nil
 }
+
+func (bc *Blockchain) GetLastBlockFromDB(hash common.Hash, number uint64) *Block {
+	query.GetLastBlock()
+	return nil
+}
