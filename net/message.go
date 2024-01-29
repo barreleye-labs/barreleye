4c8c1a4501cae52944b8a87bfc6a167255797263
k930503 <k930503@gmail.com>
2024-01-29 21:35:23 +0900

    Rename: net

---
+++ net/message.go
@@ -0,0 +1,25 @@
+package net
+
+import (
+	"github.com/barreleye-labs/barreleye/core/types"
+)
+
+type GetBlocksMessage struct {
+	From uint32
+	// If to is 0 the maximum blocks will be returned.
+	To uint32
+}
+
+type BlocksMessage struct {
+	Blocks []*types.Block
+}
+
+type GetStatusMessage struct {
+}
+
+type StatusMessage struct {
+	// the id of the server
+	ID            string
+	Version       uint32
+	CurrentHeight uint32
+}
---
+++ net/rpc.go
@@ -0,0 +1,143 @@
+package net
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"fmt"
+	"github.com/barreleye-labs/barreleye/core/types"
+	"io"
+	"net"
+
+	"github.com/sirupsen/logrus"
+)
+
+type MessageType byte
+
+const (
+	MessageTypeTx        MessageType = 0x1
+	MessageTypeBlock     MessageType = 0x2
+	MessageTypeGetBlocks MessageType = 0x3
+	MessageTypeStatus    MessageType = 0x4
+	MessageTypeGetStatus MessageType = 0x5
+	MessageTypeBlocks    MessageType = 0x6
+)
+
+type RPC struct {
+	From    net.Addr //string
+	Payload io.Reader
+}
+
+type Message struct {
+	Header MessageType
+	Data   []byte
+}
+
+func NewMessage(t MessageType, data []byte) *Message {
+	return &Message{
+		Header: t,
+		Data:   data,
+	}
+}
+
+func (msg *Message) Bytes() []byte {
+	buf := &bytes.Buffer{}
+	gob.NewEncoder(buf).Encode(msg)
+	return buf.Bytes()
+}
+
+type DecodedMessage struct {
+	From net.Addr
+	Data any
+}
+
+type RPCDecodeFunc func(RPC) (*DecodedMessage, error)
+
+func DefaultRPCDecodeFunc(rpc RPC) (*DecodedMessage, error) {
+	msg := Message{}
+	if err := gob.NewDecoder(rpc.Payload).Decode(&msg); err != nil {
+		return nil, fmt.Errorf("failed to decode message from %s: %s", rpc.From, err)
+	}
+
+	// fmt.Printf("receiving message: %+v\n", msg)
+
+	logrus.WithFields(logrus.Fields{
+		"from": rpc.From,
+		"type": msg.Header,
+	}).Debug("new incoming message")
+
+	switch msg.Header {
+	case MessageTypeTx:
+		tx := new(types.Transaction)
+		if err := tx.Decode(types.NewGobTxDecoder(bytes.NewReader(msg.Data))); err != nil {
+			return nil, err
+		}
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: tx,
+		}, nil
+
+	case MessageTypeBlock:
+		block := new(types.Block)
+		if err := block.Decode(types.NewGobBlockDecoder(bytes.NewReader(msg.Data))); err != nil {
+			return nil, err
+		}
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: block,
+		}, nil
+
+	case MessageTypeGetStatus:
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: &GetStatusMessage{},
+		}, nil
+
+	case MessageTypeStatus:
+		statusMessage := new(StatusMessage)
+		if err := gob.NewDecoder(bytes.NewReader(msg.Data)).Decode(statusMessage); err != nil {
+			return nil, err
+		}
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: statusMessage,
+		}, nil
+
+	case MessageTypeGetBlocks:
+		getBlocks := new(GetBlocksMessage)
+		if err := gob.NewDecoder(bytes.NewReader(msg.Data)).Decode(getBlocks); err != nil {
+			return nil, err
+		}
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: getBlocks,
+		}, nil
+
+	case MessageTypeBlocks:
+		blocks := new(BlocksMessage)
+		if err := gob.NewDecoder(bytes.NewReader(msg.Data)).Decode(blocks); err != nil {
+			return nil, err
+		}
+
+		return &DecodedMessage{
+			From: rpc.From,
+			Data: blocks,
+		}, nil
+
+	default:
+		return nil, fmt.Errorf("invalid message header %x", msg.Header)
+	}
+
+}
+
+type RPCProcessor interface {
+	ProcessMessage(*DecodedMessage) error
+}
+
+func init() {
+	gob.Register(elliptic.P256())
+}
