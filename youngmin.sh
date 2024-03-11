0966faad9572162546205321e92c35a9e5b2bde1
k930503 <k930503@gmail.com>
2024-03-12 00:49:47 +0900

    Feat: Use docker host volume

--- youngmin.sh
+++ youngmin.sh
@@ -5,5 +5,7 @@ peers="localhost:4101"
 #peers="172.31.8.44:4101"
 httpPort="9002"
 key="f2e1e4331b10c2b84a8ed58226398f5d11ee78052afa641d16851bd66bbdadb7"
-docker run -d -it --name ${name} --net host -v /data:/data kym6772/barreleye:1.0.0 /barreleye/bin/barreleye -name=${name} -role=${role} -port=${port} -peers=${peers} -http.port=${httpPort} -key=${key}
-#docker run -d -it --name ${name} -p ${port}:${port} -p ${httpPort}:${httpPort} -v /data:/data kym6772/barreleye:1.0.0 /barreleye/bin/barreleye -name=${name} -role=${role} -port=${port} -peers=${peers} -http.port=${httpPort} -key=${key}
+hostDataDir="/data/youngmin"
+containerDataDir="/barreleye/barreldb/youngmin"
+
+docker run -d -it --name ${name} --net host -v ${hostDataDir}:${containerDataDir} kym6772/barreleye:1.0.0 /barreleye/bin/barreleye -name=${name} -role=${role} -port=${port} -peers=${peers} -http.port=${httpPort} -key=${key}
