6dfb01ad97d955fdff76fde946482d315e468f67
k930503 <k930503@gmail.com>
2025-05-25 11:07:04 +0900

    edit: docker tag

--- barreleye.sh
+++ barreleye.sh
@@ -7,4 +7,4 @@ key=
 hostDataDir="/data/barreleye"
 containerDataDir="/barreleye/barreldb/barreleye"
 
-docker run -d -it --name ${name} --net host -v ${hostDataDir}:${containerDataDir} kym6772/barreleye:1.0.0 /barreleye/bin/barreleye -name=${name} -role=${role} -port=${port} -peers=${peers} -http.port=${httpPort} -key=${key}
+docker run -d -it --name ${name} --net host -v ${hostDataDir}:${containerDataDir} kym6772/barreleye:v1.0.0 /barreleye/bin/barreleye -name=${name} -role=${role} -port=${port} -peers=${peers} -http.port=${httpPort} -key=${key}
